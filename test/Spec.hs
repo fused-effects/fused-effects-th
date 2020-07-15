4bb911440752645dae9c852816d5223a5a97c202
Patrick Thomson <patrickt@github.com>
2020-07-15 11:02:55 -0400

    Test TH errors.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# OPTIONS_GHC -ddump-splices #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main
   ( main,
@@ -11,13 +11,45 @@ where
 import Control.Carrier.State.Strict (evalState)
 import Control.Effect.State hiding (get, put)
 import Control.Effect.TH
+import Control.Exception (SomeException, try)
+import Data.Either
+import Language.Haskell.TH (runQ)
+import System.IO
 import Test.Tasty
 import Test.Tasty.HUnit
 
 makeEffectDefinitions ''State
 
+assertThrows :: String -> IO a -> Assertion
+assertThrows msg go =
+  fmap isLeft (try @SomeException go) >>= assertBool msg
+
+testState :: TestTree
+testState = testCase "Generated State functions" $ do
+  assertEqual "get" (run $ evalState "hello" get) "hello"
+  assertEqual "get-put" (run $ evalState "bad" (put "good" *> get)) "good"
+
+data BadGADT where
+  BadGADT :: BadGADT
+
+testDataErrors :: TestTree
+testDataErrors =
+  testCase "Bad data types" $ do
+    assertThrows "Bool" (runQ (makeEffectDefinitions ''Bool))
+    assertThrows "Ill-formed GADT" (runQ (makeEffectDefinitions ''BadGADT))
+
 main :: IO ()
-main = defaultMain $ do
-  testCase "Generated State functions" $ do
-    assertEqual "get" (run $ evalState "hello" get) "hello"
-    assertEqual "get-put" (run $ evalState "bad" (put "good" *> get)) "good"
+main = do
+  hClose stderr -- silences TH warnings
+  defaultMain $
+    testGroup
+      "Language.Haskell.TH"
+      [ testGroup
+          "Behavior"
+          [ testState
+          ],
+        testGroup
+          "Template Haskell errors"
+          [ testDataErrors
+          ]
+      ]
