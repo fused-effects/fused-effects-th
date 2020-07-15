80b5fe336c5e05a31651abcb2e3c92f5b78b7c33
Patrick Thomson <patrickt@github.com>
2020-07-15 10:39:45 -0400

    Docs.

--- src/Control/Effect/TH.hs
+++ src/Control/Effect/TH.hs
@@ -15,7 +15,7 @@ import Data.Foldable
 import Data.Traversable
 import Language.Haskell.TH as TH
 
-data Blueprint = Blueprint
+data PerEffect = PerEffect
   { typeName :: TH.Name,
     effectTypeVars :: [TH.TyVarBndr],
     monadTypeVar :: TH.TyVarBndr,
@@ -27,9 +27,40 @@ data PerDecl = PerDecl
     functionName :: TH.Name,
     ctorArgs :: [TH.Type],
     returnType :: TH.Type,
-    blueprint :: Blueprint
+    perEffect :: PerEffect
   }
 
+-- | Given an effect type, this splice generates functions that invoke its constructors, using 'send'.
+--
+-- That is to say, given the standard @State@ type
+--
+-- @
+--   data State s m k where
+--     Get :: State s m s
+--     Put :: s -> State s m ()
+-- @
+--
+-- an invocation of @makeEffectDefinitions ''State@ will generate code that looks like
+--
+-- @
+--   get ::
+--     forall (s :: Type) sig (m :: Type -> Type).
+--     Has (State s) sig m =>
+--     m s
+--   get = send Get
+--   {-# INLINEABLE get #-}
+---   put ::
+--     forall (s :: Type) sig (m :: Type -> Type).
+--     Has (State s) sig m =>
+--     s ->
+--     m ()
+--   put a = send (Put a)
+--   {-# INLINEABLE put #-}
+-- @
+--
+-- The type variables in each declared function signature will appear in the order
+-- they were defined in the state type.
+--
 makeEffectDefinitions :: Name -> TH.DecsQ
 makeEffectDefinitions typ =
   TH.reify typ >>= \case
@@ -40,11 +71,11 @@ makeEffectDefinitions typ =
         _ -> fail ("Effect types need at least two type arguments: a monad `m` and continuation `k`.")
       -- Continue, recording the various relevant data from the type in question.
       let effectTypeVars = effectTypeVarsWithoutSig ++ [TH.PlainTV (mkName "sig")]
-      join <$> traverse (\forallConstructor -> makeDeclaration Blueprint {..}) cons
+      join <$> traverse (\forallConstructor -> makeDeclaration PerEffect {..}) cons
     other -> fail ("Can't generate definitions for a non-data-constructor: " <> pprint other)
 
-makeDeclaration :: Blueprint -> TH.DecsQ
-makeDeclaration blueprint@Blueprint {..} = do
+makeDeclaration :: PerEffect -> TH.DecsQ
+makeDeclaration perEffect@PerEffect {..} = do
   (names, ctorArgs, returnWithResult) <- case forallConstructor of
     TH.ForallC _vars _ctx (TH.GadtC names bangtypes returnType) ->
       pure (names, fmap snd bangtypes, returnType)
@@ -81,7 +112,7 @@ makeClause PerDecl {..} = TH.clause pats body []
     names = fmap (mkName . pure) (take (length ctorArgs) ['a' .. 'z'])
 
 makeSignature :: PerDecl -> TH.DecQ
-makeSignature PerDecl {blueprint = Blueprint {..}, ..} =
+makeSignature PerDecl {perEffect = PerEffect {..}, ..} =
   let sigVar = last effectTypeVars
       rest = init effectTypeVars
       getTyVar = \case
